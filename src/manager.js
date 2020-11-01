f56971b3d17aeb6bedf98bd763c6a66b0bf66c5c
Lars den Bakker <larsdenbakker@gmail.com>
2020-11-01 12:51:20 +0100

    Export React in preview

--- src/manager.js
+++ src/manager.js
@@ -1,6 +1,6 @@
 import '@storybook/core/dist/client/manager/index.js';
-import react from 'react';
+import React from 'react';
 import * as addons from '@storybook/addons';
 import * as coreEvents from '@storybook/core-events';
 
-export { react, addons, coreEvents };
+export { React, addons, coreEvents };
--- src/preact.js
+++ src/preact.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import * as addons from '@storybook/addons';
 import * as coreEvents from '@storybook/core-events';
 import * as addonDocsBlocks from '@storybook/addon-docs/blocks.js';
 import { registerPreviewEntry } from './registerPreviewEntry.js';
 
-export { coreEvents, addons, registerPreviewEntry, addonDocsBlocks };
+export { React, coreEvents, addons, registerPreviewEntry, addonDocsBlocks };
 
 export {
   storiesOf,
