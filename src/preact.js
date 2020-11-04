25026e4773d606e9ffcb83c8c9479bcb45a1dab4
Lars den Bakker <larsdenbakker@gmail.com>
2020-11-04 23:26:13 +0100

    Export mdx

--- src/preact.js
+++ src/preact.js
@@ -2,9 +2,10 @@ import React from 'react';
 import * as addons from '@storybook/addons';
 import * as coreEvents from '@storybook/core-events';
 import * as addonDocsBlocks from '@storybook/addon-docs/blocks.js';
+import { mdx } from '@mdx-js/react';
 import { registerPreviewEntry } from './registerPreviewEntry.js';
 
-export { React, coreEvents, addons, registerPreviewEntry, addonDocsBlocks };
+export { React, coreEvents, addons, registerPreviewEntry, addonDocsBlocks, mdx };
 
 export {
   storiesOf,
