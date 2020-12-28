8d2b9e920a955120e2286c24d39863b0c372d277
Lars den Bakker <larsdenbakker@gmail.com>
2020-12-28 17:25:36 +0100

    Remove built files

--- theming/create.js
+++
@@ -1,2 +0,0 @@
-import"../dist/storybook-prebuilt-2d1aa0cd.js";import"../dist/storybook-prebuilt-2e0ce80e.js";import"../dist/storybook-prebuilt-b7996f24.js";import{c as t}from"../dist/storybook-prebuilt-ebb67402.js";var o=t,r=o.create,e=o.themes;export{r as create,e as themes};
-//# sourceMappingURL=create.js.map
