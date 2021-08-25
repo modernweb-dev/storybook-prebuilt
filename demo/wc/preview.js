ee09001c10bbdd563b92b89d986f51b411bf2cb5
Westbrook Johnson <westbrook.johnson@gmail.com>
2021-08-25 10:24:08 -0400

    Use alpha Storybook to make an alpha prebuilt

--- demo/wc/preview.js
+++ demo/wc/preview.js
@@ -1,3 +1,11 @@
+import {
+  setCustomElements,
+} from '../../web-components.js';
+// @TODO: update WDS config for importing this data and leveraging it in `setCustomElements()`.
+// import customElements from './custom-elements.json';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+setCustomElements({});
